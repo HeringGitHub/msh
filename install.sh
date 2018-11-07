d882883ed484e92be66e23f8d9011e23c60a1646
hering <heringlinux@yeah.net>
2018-11-07 10:15:04 +0800

    Modified when inpute a string that is not digit.

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if [ x"$1" == x"-f" ]
+then
+	cp -f ./msh /usr/bin
+	chmod +x /usr/bin/msh
+	echo "Install completed!"
+	exit
+fi
+
 if [ ! -f "/usr/bin/msh" ]
 then
 	cp ./msh /usr/bin
@@ -7,6 +16,6 @@ then
 else
 	echo "Install failed."
 	echo "There is a package has same name with msh."
-	echo "Please modify the msh name and install.sh related code."
+	echo "Please modify the msh name and install.sh related code or use -f option force to install."
 fi
 
